ab5c3efd9175fad9f5ff032a0f6573e36cab27e6
大数加 達郎 <oscarwild13@gmail.com>
2017-07-30 13:58:59 +0900

    .gitignoreの設定と静的ページのroutes設定

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def top
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#top'
+
+  get 'static_pages/about'
+
+  get 'static_pages/terms'
+
+  get 'static_pages/privacy'
+
+  root 'statoc_pages#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
